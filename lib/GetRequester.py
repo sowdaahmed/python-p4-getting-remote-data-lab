baa460176e57091db3ec6a524752b2413b933a82
Sowda Ahmed <ayanmoha4949@gmail.com>
2025-07-22 00:39:47 -0700

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,16 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Returns raw body (bytes)
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        try:
+            return response.json()  # Parses and returns Python list/dict
+        except json.JSONDecodeError:
+            return None
